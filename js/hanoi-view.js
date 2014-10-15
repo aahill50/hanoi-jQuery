7312de2e85700423992c9992d5cdae1c2c734b35
App Academy Student <student@appacademy.io>
2014-10-15 13:27:11 -0400

    upside down rendering

--- js/hanoi-view.js
+++ js/hanoi-view.js
@@ -10,16 +10,37 @@
 
   View.prototype.render = function () {
     $('.stack').each(function () {
-      this.empty();
+      $(this).empty();
     });
 
     var game = this.game
-     $('.stack').each(function (i) {
+    $('.stack').each(function (i) {
       var stack = game.towers[i];
-      $(this).append()
-    };
+      for (var i = 0; i < 3; i++) {
+        if (stack[i]) {
+          $(this).append("<li class='disk-" + stack[i] + "'></li>")
+        } else {
+          $(this).append("<li class='empty'></li>")
+        }
+      };
+    });
+  };
+
+  View.prototype.installClickHandle = function () {
+    $('.hanoi').on('click', '.stack', function (event) {
+      if($('.from').length === 0) {
+        $(this).addClass("from");
+      }
+      else {
+        var fromStackIdx = $('.from').data('id')
+        var toStackIdx = $(this).data('id')
+        console.log(fromStackIdx)
+        console.log(toStackIdx)
+
+      }
+    });
   };
 
 
 
-}(););
+}());
